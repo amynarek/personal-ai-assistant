cf4228d83a162b09342df4d07ab573547af56ff5
amynarek <40737986+amynarek@users.noreply.github.com>
2025-04-07 18:25:24 +0200

    Telegram integration

---
+++ Nerdboard.PersonalAI/Nerdboard.PersonalAI.App/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+using var cts = new CancellationTokenSource();
+
+var bot = new TelegramBotClient(config["Telegram:ApiKey"], cancellationToken: cts.Token);
+var me = await bot.GetMe();
+
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"Jestem {me.FirstName}. I słucham :)");
+Console.ReadLine();
+cts.Cancel();
+
+
+async Task OnMessage(Telegram.Bot.Types.Message message, Telegram.Bot.Types.Enums.UpdateType type)
+{
+    if (message.Text is null) 
+        return;	
+
+    Console.WriteLine($"Otrzymana wiadomość typu {type} '{message.Text}' in {message.Chat}");
+
+    await bot.SendMessage(message.Chat, $"Otrzymałem wiadomość: {message.Text}");
+}
