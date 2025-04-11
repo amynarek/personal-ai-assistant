9ef942245b85c0db191527e6639a07d68e5107cf
amynarek <40737986+amynarek@users.noreply.github.com>
2025-04-11 17:26:03 +0200

    PR fixes

--- Nerdboard.PersonalAI/Nerdboard.PersonalAI.App/Utils/StringExtensions.cs
+++ Nerdboard.PersonalAI/Nerdboard.PersonalAI.App/Utils/StringExtensions.cs
@@ -1,18 +1,6 @@
 public static class StringExtensions
 {
-    public static bool IsNullOrEmpty(this string s)
-    {
-        if (s == null)
-            throw new ArgumentNullException(nameof(s));
+    public static bool IsNullOrEmpty(this string s) => string.IsNullOrEmpty(s);
 
-        return string.IsNullOrEmpty(s);
-    }
-
-    public static bool IsNullOrWhitespace(this string s)
-    {
-        if (s == null)
-            throw new ArgumentNullException(nameof(s));
-
-        return string.IsNullOrWhiteSpace(s);
-    }
+    public static bool IsNullOrWhitespace(this string s) => string.IsNullOrWhiteSpace(s);
 }
